4622ac2c2be6c91a45ab9cfe9bc3adf35cb33a2a
Realdo Dias <realdo@omesti.com>
2020-08-11 00:23:47 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE
+  .replace('<DB_USERNAME>', process.env.DB_USERNAME)
+  .replace('<DB_PASSWORD>', process.env.DB_PASSWORD)
+  .replace('<DB_CLOUD>', process.env.DB_CLOUD)
+  ;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Successfully connected!');
+  });
+  
+  const port = process.env.PORT || 3030;
+  app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
