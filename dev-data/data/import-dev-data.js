a16359427e554738ee735cdf651c978ab595bd0a
Realdo Dias <realdo@omesti.com>
2020-08-11 20:30:13 +0800

    add functions to import and delete data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-process-exit */
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Tour = require("../../models/tourModel");
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE
+  .replace('<DB_USERNAME>', process.env.DB_USERNAME)
+  .replace('<DB_PASSWORD>', process.env.DB_PASSWORD)
+  .replace('<DB_CLOUD>', process.env.DB_CLOUD)
+  ;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Successfully connected!');
+  });
+
+// read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded to the database!');
+  } catch (err) {
+    console.log(`Error importing data to the database: ${err}`);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted from the database!');
+  } catch (err) {
+    console.log(`Error deleting data from the database: ${err}`);
+  }
+  process.exit();
+};
+
+if(process.argv[2] === '--import') {
+  importData();
+} else if(process.argv[2] === '--delete') {
+  deleteData();
+}
+
+// console.log(process.argv);
+  
