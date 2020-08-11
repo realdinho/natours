b787d779855c366522210e12fcb194de7d2feaa9
Realdo Dias <realdo@omesti.com>
2020-08-12 01:47:25 +0800

    add pagination in queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,14 +13,14 @@ exports.getAllTours = async (req, res) => {
     // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    // console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
-
+    
     // 2) Sorting
     if(req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
+      // console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
@@ -32,6 +32,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // 4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if(req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if(skip >= numTours) throw new Error('This page does not exist!')
     }
 
     // execute query
