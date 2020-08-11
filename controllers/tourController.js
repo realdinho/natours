3751fc23c7b890d2027dc2ab60c02319cf7591ff
Realdo Dias <realdo@omesti.com>
2020-08-12 00:47:22 +0800

    add filters in queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,26 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build Query
+    // 1) Filtering
+    const queryObj = {...req.query};
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach(el => delete queryObj[el]);
+    // console.log(req.query, queryObj);
+
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+    const tours = await query; // execute query
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
   
     res.status(200).json({
       status: 'success',
