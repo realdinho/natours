e85478b1ac24941ed011d63c31546611461a740c
Realdo Dias <realdo@omesti.com>
2020-08-12 16:26:14 +0800

    aggregation  middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -99,6 +99,13 @@ tourSchema.post(/^find/, function(docs, next) {
   next();
 });
 
+// aggregation middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true }}});
+  console.log(this.pipeline());
+  next();
+})
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
