8484c1e4aeacbb59deab8fade82202618e1bb7b7
Realdo Dias <realdo@omesti.com>
2020-08-12 12:42:10 +0800

    refactoring API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
 const Tour = require("../models/tourModel");
+const ApiFeatures = require('../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,57 +10,15 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Build Query
-    // 1A) Filtering
-    const queryObj = {...req.query};
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach(el => delete queryObj[el]);
-    // console.log(req.query, queryObj);
-
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-    
-    // 2) Sorting
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) Field limiting
-    if(req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if(skip >= numTours) throw new Error('This page does not exist!')
-    }
-
     // execute query
-    const tours = await query; 
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
+    const features = new ApiFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query; 
   
+    // send response
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
@@ -79,8 +37,6 @@ exports.getAllTours = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    // const newTour = new Tour();
-    // newTour.save();
     const newTour = await Tour.create(req.body);
   
     res.status(201).json({
@@ -100,7 +56,6 @@ exports.createTour = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-    // Tour.findOne({ _id: req.params.id });
 
     res.status(200).json({
       status: 'success',
@@ -150,90 +105,3 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
-
-
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkId = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   const id = +req.params.id;
-
-//   if (id > tours.length) {
-//     return res.status(404).send({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'Missing name or price',
-//     });
-//   }
-//   next();
-// };
-
-// exports.getAllTours = (req, res) => {
-//   res.status(200).json({
-//     status: 'success',
-//     requestedAt: req.requestTime,
-//     results: tours.length,
-//     data: {
-//       tours,
-//     },
-//   });
-// };
-
-// exports.createTour = (req, res) => {
-//   const newId = tours[tours.length - 1].id + 1;
-//   const newTour = { id: newId, ...req.body };
-
-//   tours.push(newTour);
-//   fs.writeFile(
-//     `${__dirname}/dev-data/data/tours-simple.json`,
-//     JSON.stringify(tours),
-//     () => {
-//       res.status(201).json({
-//         status: 'success',
-//         data: {
-//           tour: newTour,
-//         },
-//       });
-//     }
-//   );
-//   // res.send('Done');
-// };
-
-// exports.getTour = (req, res) => {
-//   const id = +req.params.id;
-//   const tour = tours.find((el) => el.id === id);
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   });
-// };
-
-// exports.updateTour = (req, res) => {
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour: '<Updated tour here>',
-//     },
-//   });
-// };
-
-// exports.deleteTour = (req, res) => {
-//   res.status(204).json({
-//     status: 'success',
-//     data: null,
-//   });
-// };
