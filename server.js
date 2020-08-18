cc45f10a66a6f3f0b0dcb31422a58ccb819b40ef
Realdo Dias <realdo@omesti.com>
2020-08-19 04:34:43 +0800

    handling uncaught exceptions

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-exit */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -6,22 +7,36 @@ const app = require('./app');
 
 
 const DB = process.env.DATABASE
-  .replace('<DB_USERNAME>', process.env.DB_USERNAME)
-  .replace('<DB_PASSWORD>', process.env.DB_PASSWORD)
-  .replace('<DB_CLOUD>', process.env.DB_CLOUD)
-  ;
+.replace('<DB_USERNAME>', process.env.DB_USERNAME)
+.replace('<DB_PASSWORD>', process.env.DB_PASSWORD)
+.replace('<DB_CLOUD>', process.env.DB_CLOUD)
+;
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log('DB Successfully connected!');
+.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+.then(() => console.log('DB Successfully connected!'));
+
+const port = process.env.PORT || 3030;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLER REJECTION! 🤯 Shutting down..');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
   });
-  
-  const port = process.env.PORT || 3030;
-  app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+});
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT REJECTION! 🤯 Shutting down..');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
   });
+})
