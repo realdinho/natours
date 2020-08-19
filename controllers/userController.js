0251ffc69e8261b3ccc9955c2e62afa4fe3f0f1f
Realdo Dias <realdo@omesti.com>
2020-08-19 18:06:22 +0800

    add jwt, implement APIs to get all users and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not defined yet'
-  })
-};
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
+  });
+  next();
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true, 'Please provide a password'],
-    minlength: 8
+    minlength: 8,
+    select: false
   },
   passwordConfirm: {
     type: String,
@@ -44,6 +45,10 @@ userSchema.pre('save', async function(next) {
   this.passwordConfirm = undefined;
   return next();
 });
+
+userSchema.methods.correctPassword = function(candidatePassword, userPassword) {
+  return bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
