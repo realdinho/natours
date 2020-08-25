5d3f01c3f0661964fc2fca9fc61cf5fdbb27fbff
Realdo Dias <realdo@omesti.com>
2020-08-26 03:20:10 +0800

    add token to cookies and limit API requests from an IP address

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,20 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  // cookies
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWS_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined; // remove password from the output
+
   res.status(statusCode).json({
     status: 'success',
     token,
