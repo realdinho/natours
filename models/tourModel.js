733a1f49a0dcb3643957cf34035701480ecf2ec1
Realdo Dias <realdo@omesti.com>
2020-08-26 21:57:39 +0800

    modeling tour guides using embedding and child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel'); // embedding
 
 const tourSchema = new mongoose.Schema(
   {
@@ -103,6 +104,13 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
+    // guides: Array, // for embedding
   },
   {
     toJSON: { virtuals: true },
@@ -120,6 +128,13 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// embedding
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
