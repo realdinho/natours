cf2305c47bcf2d5f5f5645522670e8ebc9b8ef24
Realdo Dias <realdo@omesti.com>
2020-08-29 13:55:55 +0800

    add missing authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,24 +6,19 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// protect all routes after this line since middlewares run in sequence
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// restrict access to the next APIs for all users except admin
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
