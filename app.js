833c85bbd06c38c890ae791312640a25b86193a6
Realdo Dias <realdo@omesti.com>
2020-09-18 23:47:04 +0800

    install compression and update gitignore file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-
+const compression = require('compression');
 // const cors = require('cors');
 
 const AppError = require('./utils/appError');
@@ -70,6 +70,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 // test middleware
 app.use((req, res, next) => {
