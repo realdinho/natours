cde115400905d9cb8d36bce0cc4dc38e5eedad1d
Realdo Dias <realdo@omesti.com>
2020-09-18 15:56:39 +0800

    build email handler

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1. create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Realdo Dias <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2. define email options
-  const mailOptions = {
-    from: 'Realdo Dias <rdias@realdiv.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: ,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
 
-  // 3. actually send the email
-  await transporter.sendMail(mailOptions);
+    // create a transporter
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // send the actual email
+  async send(template, subject) {
+    // render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../../views/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours App!');
+  }
 };
-
-module.exports = sendEmail;
