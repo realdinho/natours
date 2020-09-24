3b05957048a4455944d5f0f82fc5216121676727
Realdo Dias <realdo@omesti.com>
2020-09-24 12:00:46 +0800

    stripe alert messages - fix bug 2

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,12 +4,13 @@ const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-exports.alert = (req, res, next) => {
+exports.alerts = (req, res, next) => {
   const { alert } = req.query;
   if (alert === 'booking') {
     res.locals.alert =
       "Your booking was successfull. Please check your email for confirmation. If your booking doesn't show up immediately, please come back later.";
   }
+  next();
 };
 
 exports.getOverview = catchAsync(async (req, res, next) => {
